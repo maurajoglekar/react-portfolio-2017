05a3812bfb27386f9949e615a78df1df1b4158a3
maura <maura_jj@sbcglobal.net>
2017-08-23 09:44:36 -0700

    initial SPA

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import {Link} from 'react-router'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>React SPA</h2>
+          <h3>by</h3>
+          <h3>Maura Joglekar</h3>
+        </div>
+        <p className="App-intro">
+          React Router!
+        </p>
+
+        
+      </div>
+    );
+  }
+}
+
+export default App;
