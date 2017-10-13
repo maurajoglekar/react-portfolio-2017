9a704ca160626356da8149d002b9d4a739715c6e
maura <maura_jj@sbcglobal.net>
2017-10-12 17:45:11 -0700

    add send email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
 import './App.css';
+import sendgrid from 'sendgrid'
 
 const Contact = createReactClass({
     sendEmail: function(e) {
+        var helper = sendgrid.mail;
+        var from_email = new helper.Email('test@example.com');
+        var to_email = new helper.Email('maura_jj@sbcglobal.net');
+        var subject = 'Hello World from the SendGrid Node.js Library!';
+        var content = new helper.Content('text/plain', 'Hello, Email!');
+        var mail = new helper.Mail(from_email, subject, to_email, content);
+
+        var sg = sendgrid(process.env.SENDGRID_API_KEY);
+        var request = sg.emptyRequest({
+          method: 'POST',
+          path: '/v3/mail/send',
+          body: mail.toJSON(),
+        });
+
+        sg.API(request, function(error, response) {
+          console.log(response.statusCode);
+          console.log(response.body);
+          console.log(response.headers);
+        });
         alert('Need to implement sending contact email!')
      },    
     render: function() {
