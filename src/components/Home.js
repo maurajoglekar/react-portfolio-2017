f4ff0ab919cb261a4239254ef1e503c4588d375b
maura <maura_jj@sbcglobal.net>
2018-06-19 00:23:49 -0700

    changed tooltips to use react tooltip

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip'
 import {NavLink } from 'react-router-dom'
 import rateColor from './images/rateColor.jpg'
 import color2 from './images/colorSquare.jpg'
@@ -20,14 +21,13 @@ const Home = () => (
           </div>
         </div>
 
+        <ReactTooltip />
+
         <div className="row container-fluid ">
         <div className="col-sm-4">
           <div className="thumbnail">
             <div className="mycontainer">
-               <h3>Git Hub Search</h3>
-               <div className="overlay col-sm-4">
-                <div className="text">A hacker rank test that allows the user to search for repos using the Github Search API. </div>
-              </div>
+               <h3 data-tip="A hacker rank test that allows the user to search for repos using the Github Search API.">Git Hub Search</h3>
             </div>
             <NavLink to='/ghSearchBox'><img src={ghsearch} alt="GitHub Search" width="300" height="300" /></NavLink>        
           </div> 
@@ -35,10 +35,7 @@ const Home = () => (
          <div className="col-sm-4">
           <div className="thumbnail">
             <div className="mycontainer">
-               <h3>Review Movies</h3>
-               <div className="overlay">
-                <div className="text">Allows the user to keep a list of movie ratings with actions to add, view, remove, and update the movie reviews. </div>
-              </div>
+               <h3 data-tip="Allows the user to keep a list of movie ratings with actions to add, view, remove, and update the movie reviews. ">Review Movies</h3>
             </div>
             <NavLink to='/cine'><img src={cine} alt="excel" width="300" height="300" /></NavLink>        
           </div>      
@@ -46,10 +43,7 @@ const Home = () => (
          <div className="col-sm-4">
           <div className="thumbnail">
             <div className="mycontainer">
-               <h3>Excel application</h3>
-               <div className="overlay">
-                <div className="text">Allows the user to edit the contents of a data table, and also sort, search (filter), and export the data as downloadable files.</div>
-              </div>
+               <h3 data-tip="Allows the user to edit the contents of a data table, and also sort, search (filter), and export the data as downloadable files.">Excel application</h3>
             </div>
             <NavLink to='/excel'><img src={excel} alt="excel" width="300" height="300" /></NavLink>        
           </div>      
@@ -60,10 +54,7 @@ const Home = () => (
         <div className="col-sm-4">
           <div className="thumbnail">
             <div className="mycontainer">
-               <h3>Post-Its Board</h3>
-               <div className="overlay">
-                <div className="text">An extension on the application from Lynda.com 'Learning React.js' course, a board with post-it like notes.</div>
-              </div>
+               <h3 data-tip="An extension on the application from Lynda.com 'Learning React.js' course, a board with post-it like notes.">Post-Its Board</h3>
             </div>
             <NavLink to='/bb'><img src={postits} alt="Post-Its Board" width="300" height="300" /></NavLink>        
           </div>      
@@ -71,10 +62,7 @@ const Home = () => (
         <div className="col-sm-4">
           <div className="thumbnail">
             <div className="mycontainer">
-               <h3>Cars Inventory</h3>
-               <div className="overlay">
-                <div className="text">Demonstrates navigation from list to details using unique id in router path. </div>
-              </div>
+               <h3 data-tip="Demonstrates navigation from list to details using unique id in router path. ">Cars Inventory</h3>
             </div>
            <NavLink to='/cars'><img src={cars} alt="Cars" width="300" height="300" /></NavLink>
           </div>      
@@ -82,10 +70,7 @@ const Home = () => (
          <div className="col-sm-4">
           <div className="thumbnail">
             <div className="mycontainer">
-               <h3>Create colors and star rate them</h3>
-               <div className="overlay">
-                <div className="text">Maintains a set of colors, name it, and rate it using stars. </div>
-              </div>
+               <h3 data-tip="Maintains a set of colors, name it, and rate it using stars. ">Create colors and star rate them</h3>
             </div>
             <NavLink to='/colorfactory'><img src={rateColor} alt="color factory" width="300" height="300" /></NavLink>        
           </div>      
@@ -96,10 +81,7 @@ const Home = () => (
          <div className="col-sm-4">
           <div className="thumbnail">
             <div className="mycontainer">
-               <h3>Create a To Do list</h3>
-               <div className="overlay">
-                <div className="text">Provides a form to add tasks and delete them as completed.</div>
-              </div>
+               <h3 data-tip="Provides a form to add tasks and delete them as completed.">Create a To Do list</h3>
             </div>
             <NavLink to='/todo'><img src={todolist} alt="To Do List" width="300" height="300" /></NavLink>        
           </div>      
@@ -107,11 +89,8 @@ const Home = () => (
         <div className="col-sm-4">
           <div className="thumbnail">
             <div className="mycontainer">
-               <h3>Color a square</h3>
-               <div className="overlay">
-                <div className="text">Allows the user to input a CSS color value and use that as the background color for the square. </div>
-              </div>
-            </div>
+               <h3 data-tip="Allows the user to input a CSS color value and use that as the background color for the square.">Color a square</h3>
+           </div>
            <NavLink to='/colorizer'><img src={color2} alt="colorizer" width="300" height="300" /></NavLink>
           </div>      
        </div>
